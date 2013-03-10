43f749a988b2718695ef3c48630e1af2e7bda474
ArvinH <arvin0731@gmail.com>
2013-03-11 00:39:22 +0800

    count 1-item frequency

--- apriori_association_rule/__init__.py
+++ apriori_association_rule/__init__.py
@@ -1,24 +1,48 @@
 import string
 import re
+'''
+class Apriori:
+   def __init__(self, data, minSup, minConf):
+      self.dataset = data
+      self.transList = defaultdict(list)
+      self.freqList = defaultdict(int)
+      self.itemset = set()
+      self.highSupportList = list()
+      self.numItems = 0
+      self.prepData()             # initialize the above collections
+      self.F = defaultdict(list)
+      self.minSup = minSup
+      self.minConf = minConf 
+   '''
 def getSrcData():
-    f = open('../DataSrc/datasrc.txt','r')
-    srcdata = []
-    for lines in f:
-        line = re.split('\W+',lines)
-        srcdata.append(line)
-        
-    f.close()
-    return srcdata
+       f = open('../DataSrc/datasrc.txt','r')
+       srcdata = []
+       Can1 = dict()
+       temp = dict()   
+       for lines in f:
+          lines = lines.strip('\n')
+          line = re.split('\W+',lines)
+          for item in line:
+             if item not in temp:
+                temp[item] = 1
+             else:
+               i = temp[item]
+               temp[item] = i+1
+               print(temp)
+       
+       f.close()
+       return srcdata
 
 def confidence(self, subCount, itemCount):
-        return float(itemCount)/subCount
+         return float(itemCount)/subCount
 
 def support(self, count):
-        return float(count)/self.numItems
+         return float(count)/self.numItems
+
 
 
 def main():
-    print(getSrcData())
+   print(getSrcData())
     
 
 
